a335c8d98d3c18e1dc35f46b3d7d924295ec46b0
Aleksandr Ponimaskin <aleksandr.ponimaskin@zalando.de>
2020-12-05 08:04:12 +0100

    day 5

--- src/advent.nim
+++ src/advent.nim
@@ -1,6 +1,6 @@
 import parseopt, streams
 
-import day_1, day_2, day_3, day_4
+import day_5
 
 when isMainModule:
   var parser = parseopt.initOptParser()
@@ -8,4 +8,4 @@ when isMainModule:
   let filename = parser.key
   let strm = newFileStream(filename, fmRead)
 
-  fourthDay(strm)
+  fifthDay(strm)
---
+++ src/day_5.nim
@@ -0,0 +1,61 @@
+import streams, algorithm
+from strformat import fmt
+
+proc fifthDay*(stream: FileStream) =
+  var
+    line = ""
+    max = 0
+    occupied: seq[int]
+
+  while stream.readLine(line):
+    var
+      row_h = 127
+      row_l = 0
+      seat_h = 7
+      seat_l = 0
+      row = 0
+      seat = 0
+
+    for r in line[0 .. 6]:
+      if r == 'B':
+        row_l = row_l + (row_h - row_l) div 2 + 1
+        row = row_h
+
+      else:
+        row_h = row_h - (row_h - row_l) div 2 - 1
+        row = row_l
+
+      # echo fmt"{r=} {row_l=} {row_h=} {row=}"
+        
+    for s in line[7 .. 9]:
+      if s == 'R':
+        seat_l = seat_l + (seat_h - seat_l) div 2 + 1
+        seat = seat_h
+      else:
+        seat_h = seat_h - (seat_h - seat_l) div 2 - 1
+        seat = seat_l
+        
+      # echo fmt"{s=} {seat_l=} {seat_h=} {seat=}"
+      
+    let id = row * 8 + seat
+    occupied.add(id)
+    
+    echo fmt"{line} {row=} {seat=} {id=}"
+    
+    if max < id: max = id
+
+  sort(occupied)
+
+  let 
+    maxId = occupied[high(occupied)]
+    minId = occupied[0]
+
+  var
+    mine = (maxId * (maxId + 1)  - (minId - 1) * minId) div 2
+      
+  for id in occupied:
+    mine = mine - id
+    
+  echo fmt"{max=}, {mine=}"
+
+    
